407946fc4585bbbba1fbb09af210b337d8e945e0
vonum <vonum.mk@gmail.com>
2022-07-20 12:05:07 +0200

    Deploy token contract with bank contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,20 @@
+require("dotenv").config();
+
+const {TIMEFRAME} = process.env;
+
 async function main() {
-  const timeout = 10000;
+  const initialSupply = 100000000;
   const AtracAddress = "0x98d9A611Ad1b5761bdC1dAAc42c48E4d54CF5882";
 
-  const contractFactory = await ethers.getContractFactory("./contracts/Bank.sol:Bank");
-  console.log(contractFactory);
+  const tokenContractFactory = await ethers.getContractFactory("./contracts/Token.sol:Token");
+  const tokenContract = await tokenContractFactory.deploy(initialSupply);
+  await tokenContract.deployed();
+  console.log("Token contract deployed to address:", tokenContract.address);
 
-  const contract = await contractFactory.deploy(timeout, AtracAddress);
+  const contractFactory = await ethers.getContractFactory("./contracts/Bank.sol:Bank");
+  const contract = await contractFactory.deploy(TIMEFRAME, tokenContract.address);
   await contract.deployed();
-  console.log("Contract deployed to address:", contract.address)
+  console.log("Bank contract deployed to address:", contract.address);
 }
 
 main()
